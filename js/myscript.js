0f2610c9e21e06a6278e839ff443739bddf748be
Unknown <pellirafael@gmail.com>
2018-08-27 17:51:08 -0300

    Commit para subir. Próximos passos incorporar medium e CTA.

---
+++ js/myscript.js
@@ -0,0 +1,41 @@
+
+
+ $('#eleven')
+  .prop('number', 0)
+  .animateNumber(
+    {
+      number: 12347
+    },
+    6000
+  );
+
+
+ $('#six')
+  .prop('number', 0)
+  .animateNumber(
+    {
+      number: 6500
+    },
+    6000
+  );
+
+
+ $('#cem')
+  .prop('number', 0)
+  .animateNumber(
+    {
+      number: 104
+    },
+    6000
+  );
+
+
+ $('#cidades')
+  .prop('number', 0)
+  .animateNumber(
+    {
+      number: 23
+    },
+   6000
+  );
+
